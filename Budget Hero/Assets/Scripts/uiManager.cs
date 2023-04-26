f54ce4c1906f8f28e41c9b7ec081367cf517410c
Vatvo <34244664+Vatvo@users.noreply.github.com>
2023-04-26 09:53:58 -0400

    Harnar 4/24 02
    camera and worldborder prefabs

---
+++ Budget Hero/Assets/Scripts/uiManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiManager : MonoBehaviour
+{
+
+    public GameObject healthBar;
+    public GameObject timer;
+    public GameObject moneyCounter;
+}
